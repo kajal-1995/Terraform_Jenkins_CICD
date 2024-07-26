337d2a2b0f2387e90213761a4785dce8eb48f677
kajal-1995 <122584672+kajal-1995@users.noreply.github.com>
2024-07-26 04:09:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "kajal-s3bucket-date"
+	bucket = "kajal1-s3bucket-date"
 }
 
 resource "aws_s3_bucket" "bucket2" {
@@ -12,7 +12,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "kajal-s3bucket-date"
+    bucket         = "kajal1-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "kajal-date-dblocks"
